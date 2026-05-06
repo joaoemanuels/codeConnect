c38a521a7ee5a485c33d134135ae2782d465245a
joaoemanuels <jemanuel.pi@gmail.com>
2026-05-06 11:48:39 -0300

    config spa

--- frontend/src/pages/Feed/index.jsx
+++ frontend/src/pages/Feed/index.jsx
@@ -1,8 +1,18 @@
+import { useEffect, useState } from "react";
 import { CardPost } from "../../components/CardPost";
-import { posts } from "./data";
 import styles from "./feed.module.css";
 
 export const Feed = () => {
+	const [posts, setPosts] = useState([]);
+
+	useEffect(() => {
+		fetch("http://localhost:3000/blog-posts")
+			.then((response) => {
+				return response.json();
+			})
+			.then((data) => setPosts(data));
+	}, []);
+
 	return (
 		<main className={styles.grid}>
 			{posts.map((post) => (
--- frontend/src/pages/NotFound/index.jsx
+++ frontend/src/pages/NotFound/index.jsx
@@ -1,7 +1,7 @@
 export const NotFound = () => {
 	return (
 		<main>
-			<h1 style={{ color: "white" }}>Não encontramos nada</h1>
+			<h1 style={{ color: "white" }}>404 - Não encontramos nada</h1>
 		</main>
 	);
 };
